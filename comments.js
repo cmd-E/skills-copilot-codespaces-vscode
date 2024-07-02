ebbeb19174ed513d18f689482b03f0391816ae51
cmd-E <49339376+cmd-E@users.noreply.github.com>
2024-07-02 09:58:19 +0000

    a

---
+++ comments.js
@@ -0,0 +1,15 @@
+// Import the express library
+var express = require('express');
+
+// Create an instance of express
+var app = express();
+
+// Define a route for the root URL ('/')
+app.get('/', function(req, res) {
+  res.send('Hello, world!');
+});
+
+// Start the server on port 3000
+app.listen(3000, function() {
+  console.log('Server is running on http://localhost:3000');
+});
